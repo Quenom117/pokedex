d5c9a0b42d898b65fa8eb0740f59bbae594f8851
Quenom117 <202006608@aluno.unb.br>
2021-10-27 19:18:31 -0300

    fix: adiciona key dentro do map

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Card from '../src/components/card';
 
+
 function App() {
   const [pokemons, setPokemons] = useState(null)
   const [page, setPage] = useState(1)
@@ -21,11 +22,8 @@ function App() {
     <>
       <div className='Lista'>
         {pokemons?.map((pokemon) => (
-          <Card pokemon={pokemon} />
-
+          <Card key={pokemon.id} pokemon={pokemon} />
         ))}
-
-
       </div>
       <button onClick={() => setPage(page + 1)}>
         Proxima pagina
