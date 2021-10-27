95601dda4eced855e01e41c7136c566434664bb1
Quenom117 <202006608@aluno.unb.br>
2021-10-27 19:21:22 -0300

    feat: adiciona card e modal de pokemons

--- src/components/card.js
+++ src/components/card.js
@@ -1,25 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import './card.css';
+import Modal from 'react-bootstrap/Modal';
+import styled from 'styled-components';
+import colors from '../styles/colors';
+
 
 export default function Card(pokemon) {
-    return (
-        <div className='cartao'>
-            <img src={pokemon.pokemon.image_url} />
-            <div>
-                {pokemon.pokemon.name}
-            </div>
-            <div>
-                {pokemon.pokemon.number}
-            </div>
-            <div>
-                {pokemon.pokemon.weight}
-            </div>
-            <div>
-                {pokemon.pokemon.height}
-            </div>
-            <div>
-                {pokemon.kind}
-            </div>
-        </div>
-    )
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	const tipos = pokemon.pokemon.kind.split(';')
+	const paraMaiusculo = (palavra)=>{
+		return palavra.slice(0,1).toUpperCase()+palavra.slice(1,palavra.length)
+	}
+	const Tipo = styled.div`
+		background-color:${props => colors[props.cor]};
+		margin: 0.3rem;
+		border-radius: 5px;
+		padding: 0.1rem 0.4rem;
+	`
+
+
+	return (
+		<>
+
+			<div className='cartao' onClick={handleShow}>
+				<img alt='imagem de pokemon' src={pokemon.pokemon.image_url} />
+				<div>
+					{paraMaiusculo(pokemon.pokemon.name)}
+				</div>
+				<div>
+					{pokemon.pokemon.number}
+				</div>
+				<div className='tipos'>
+					{tipos.map((tipo) => (
+						<Tipo cor={tipo}>
+							{tipo}
+						</Tipo>
+					))}
+				</div>
+			</div>
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>{pokemon.pokemon.name}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<img alt='imagem de pokemon' src={pokemon.pokemon.image_url} />
+					<div>
+						{pokemon.pokemon.name}
+					</div>
+					<div>
+						{pokemon.pokemon.number}
+					</div>
+					<div>
+						{pokemon.pokemon.weight}
+					</div>
+					<div>
+						{pokemon.pokemon.height}
+					</div>
+					<div>
+						{pokemon.pokemon.kind}
+					</div>
+				</Modal.Body>
+			</Modal>
+		</>
+
+	)
 }
---
+++ src/styles/colors.js
@@ -0,0 +1,21 @@
+const colors = {
+    bug: "#7ED578",
+    electric: "#FFF34B",
+    fairy: "#FF7EE5",
+    fighting: "#F17373",
+    fire: "#FFB433",
+    flying: "#D7F1E9",
+    ghost: "#E2E2E2",
+    grass: "#5EFF53",
+    ground: "#AA8546",
+    ice: "#AEE3FB",
+    normal: "#D7DBA8",
+    poison: "#CE52F9",
+    psychic: "#FFC157",
+    rock: "#757575",
+    steel: "#A1A1A1",
+    water: "#7192FF",
+    dragon: "#43372D",
+    dark: "#707070",
+}
+export default colors;
